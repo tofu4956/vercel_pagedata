1ac4aca9876e09ead88728fec164386ef3b501ed
tofu4956 <tofu4956@users.noreply.github.com>
2021-12-12 23:07:16 +0900

    aaa

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -19,7 +19,7 @@ const About = (): JSX.Element => {
             人間
             <br/>
             <br/>
-            <h2 className="text-xl">適当な経歴等</h2>
+            <h2 className="text-xl">適当な経歴/ゲーム成績等</h2>
             <ul>
               <li><a href="https://github.com/tofu4956/emoote">技育キャンプ2021 vol.7 - emoote (非入賞)</a></li>
               <li><a href="https://github.com/tofu4956/a_2110">JPHACKS2021 - re-move (非入賞)</a></li>
@@ -29,7 +29,7 @@ const About = (): JSX.Element => {
               <li><p>Dark Forest v0.6 Round 1 42th</p></li>
               <li><a href="https://opensea.io/assets/0x9724fdf5ae41570decc2d3094c65eafa7e1ab7d4/61107169621941629356686628367938563970564199388425286562243829952771239557971">Dark Forest v0.6 Round 2 17th</a></li>
               <li><p>Dark Forest v0.6 Round 3 51th</p></li>
-              <li><p>Dark Forest v0.6 Round 4 Astral Colossus Contributor (4th)</p></li>
+              <li><a href="https://astralcolossus.xyz/">Dark Forest v0.6 Round 4 Astral Colossus Contributor (4th)</a></li>
             </ul>
             <br/>
             <h2 className="text-xl">いろいろ</h2>
