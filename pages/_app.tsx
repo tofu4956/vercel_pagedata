53ace4d07860c4fa6e84d3973db5193fdfa7cc16
tofu4956 <tofu4956@users.noreply.github.com>
2022-02-12 06:01:21 +0900

    add quote style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import '../styles/index.css'
+import '../styles/global.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) : JSX.Element{
   return (
