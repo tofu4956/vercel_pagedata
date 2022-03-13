773e3336e17b625aab7352b08d7edffabf9f69eb
tofu4956 <tofu4956@users.noreply.github.com>
2022-03-14 05:23:08 +0900

    fix font

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,7 +3,7 @@ import DarkModeToggle from "./toggleDarkmode";
 
 const Intro = (): JSX.Element => {
   return (
-    <section className="flex flex-col items-center my-16 md:flex-row md:justify-between md:mb-12">
+    <section className="flex flex-col items-center my-16 font-sans md:flex-row md:justify-between md:mb-12">
       <h1 className="text-6xl font-bold tracking-tighter leading-tight md:pr-8 md:text-6xl">
         Blog.
       </h1>
