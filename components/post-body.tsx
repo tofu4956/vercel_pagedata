a9760c822e10ef0ce7224ef447302772f4fdcd22
tofu4956 <tofu4956@users.noreply.github.com>
2022-03-14 01:05:04 +0900

    Support MDX on demand

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -35,39 +35,13 @@ const MDXcomponents = {
 };
 
 const PostBody = ({ content }: Props): JSX.Element => {
-  const [mdxModule, setMdxModule] = useState<MDXModule>(runSync(`/*@jsxRuntime automatic @jsxImportSource react*/
-  const {jsx: _jsx} = arguments[0];
-  function MDXContent(props = {}) {
-    const {wrapper: MDXLayout} = props.components || ({});
-    return MDXLayout ? _jsx(MDXLayout, Object.assign({}, props, {
-      children: _jsx(_createMdxContent, {})
-    })) : _createMdxContent();
-    function _createMdxContent() {
-      return _jsx("div", {
-        style: {
-          justifyContent: 'center'
-        },
-        children: " Loading..."
-      });
-    }
-  }
-  return {
-    default: MDXContent
-  };`, runtime));
-  const Content = mdxModule ? mdxModule.default : Fragment;
-
-  useEffect(() => {
-    ;(async () => {
-      setMdxModule(await run(content, runtime))
-    })()
-  }, [content])
+  const mdxModule: MDXModule = runSync(content, runtime);
+  const Content = mdxModule.default
 
   return (
     <div className="max-w-3xl mx-auto">
       <div className={markdownStyles["markdown"]}>
-      <MDXProvider >
         <Content components={MDXcomponents}/>
-      </MDXProvider>
       </div>
     </div>
   );
--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -4,21 +4,12 @@ import remarkToc from "remark-toc";
 import remarkSlug from "remark-slug";
 import rehypeHighlight from "rehype-highlight";
 import remarkMdx from "remark-mdx";
-import { serialize } from "next-mdx-remote/serialize";
 import {compile} from '@mdx-js/mdx'
 import rehypeKatex from "rehype-katex";
-import rehypeReact from "rehype-react";
-import React, { ReactNode } from "react";
-import Link, { LinkProps } from "next/link";
-import {YouTube} from "../components/post/youtube"
-import {CodePen} from "../components/post/codepen"
 
 type Items = {
   [key: string]: string | string[] | boolean;
 };
-
-
-
 
 export default async function markdownToHtml(markdown: Items, data: any) {
     const result = await compile(String(markdown["content"]), {
