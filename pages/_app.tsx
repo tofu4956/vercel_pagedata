8f9afc85b5185005d730a619cbb866d1e4789ff2
tofu4956 <tofu4956@users.noreply.github.com>
2022-03-31 11:24:07 +0900

    fix cloudflare tag (change point)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,10 @@ import "../styles/index.css";
 import "../styles/global.css";
 import "highlight.js/styles/a11y-dark.css";
 import "katex/dist/katex.min.css";
-import Script from "next/script";
-import { CLOUDFLARE_TOKEN } from "../lib/constants";
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
-      <Script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon={String(`{"token": ${CLOUDFLARE_TOKEN}}`)} />
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
       </ThemeProvider>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { CLOUDFLARE_TOKEN } from "../lib/constants";
 
 export default class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="ja">
-        <Head />
+        <Head>
+          <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon={String(`{"token": ${CLOUDFLARE_TOKEN}}`)} />
+        </Head>
         <body className="dark:bg-stone-900">
           <Main />
           <NextScript />
