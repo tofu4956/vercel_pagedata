73f5742d7400c43b6a8a5c6f48be07f92081fe5b
tofu4956 <tofu4956@users.noreply.github.com>
2022-03-31 10:35:03 +0900

    fix cloudflare tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,12 @@ import "../styles/global.css";
 import "highlight.js/styles/a11y-dark.css";
 import "katex/dist/katex.min.css";
 import Script from "next/script";
+import { CLOUDFLARE_TOKEN } from "../lib/constants";
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
-      <Script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon={{"token": process.env["NEXT_PUBLIC_cltoken"]}} />
+      <Script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon={{"token": CLOUDFLARE_TOKEN}} />
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
       </ThemeProvider>
